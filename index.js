c60b7509d148a1c1ba216c6d751e3d2c050e3d86
Eros Espínola González <erosespinola@eross-mbp.lan>
2020-04-05 16:52:03 -0400

    Starter files

--- index.js
+++ index.js
@@ -17,39 +17,14 @@ const MONTHS = [
 function startTime() {
   const today = new Date();
   const h = today.getHours();
-  const m = checkTime(today.getMinutes());
-  const s = checkTime(today.getSeconds());
+  const m = today.getMinutes();
+  const s = today.getSeconds();
 
   const day = DAYS[today.getDay()];
   const month = MONTHS[today.getMonth()];
   const date = today.getDate();
   const year = today.getFullYear();
 
-  setClock(h, m, s);
-  setDate(`${day} ${month} ${date} ${year}`);
-
-  setTimeout(startTime, 1000);
-}
-
-function setClock(h, m, s) {
-  const sections = document.querySelectorAll('.number');
-  const time = [h, m, s];
-
-  sections.forEach((section, i) => {
-    const value = section.firstChild.nextSibling;
-    value.innerHTML = time[i];
-  });
-}
-
-function setDate(date) {
-  const dateWrapper = document.getElementById('date');
-  const value = dateWrapper.firstChild.nextSibling;
-  value.innerHTML = date;
-}
-
-function checkTime(number) {
-  if (number < 10) {
-    return `0${number}`;
-  }
-  return number;
+  console.log(h, m, s);
+  console.log(`${day} ${month} ${date} ${year}`);
 }
